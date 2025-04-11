8f7e19cb76b6a0792a6e33a3a3c798f43ca2cc0f
Axel B <115588628+elbarce@users.noreply.github.com>
2025-04-10 22:09:24 -0300

    Create Practica2-2.java

---
+++ Practica2-2.java
@@ -0,0 +1,291 @@
+Ejercicio 2 
+Para cada una de las siguientes situaciones, realice en forma iterativa los siguientes pasos:
+(i) indique el mal olor,
+(ii) indique el refactoring que lo corrige, 
+(iii) aplique el refactoring, mostrando el resultado final (código y/o diseño según corresponda). 
+Si vuelve a encontrar un mal olor, retorne al paso (i).
+
+2.1 Empleados
+
+public class EmpleadoTemporario {
+    public String nombre;
+    public String apellido;
+    public double sueldoBasico = 0;
+    public double horasTrabajadas = 0;
+    public int cantidadHijos = 0;
+    // ......
+    
+public double sueldo() {
+  return this.sueldoBasico + (this.horasTrabajadas * 500) + (this.cantidadHijos * 1000) - (this.sueldoBasico * 0.13);
+  }
+}
+
+public class EmpleadoPlanta {
+    public String nombre;
+    public String apellido;
+    public double sueldoBasico = 0;
+    public int cantidadHijos = 0;
+    // ......
+    
+    public double sueldo() {
+        return this.sueldoBasico + (this.cantidadHijos * 2000) - (this.sueldoBasico * 0.13);
+    }
+}
+
+public class EmpleadoPasante {
+    public String nombre;
+    public String apellido;
+    public double sueldoBasico = 0;
+    // ......
+    
+    public double sueldo() {
+        return this.sueldoBasico - (this.sueldoBasico * 0.13);
+    }
+}
+
+Resolución
+
+i) mal olor identificado:
+
+1) código duplicado: las 3 clases definidas tienen atributos idénticos en común.
+2) ruptura de encapsulamiento: los atributos no deberían aparecer públicos.
+3) métodos duplicados: las 3 clases tienen un método idéntico, que varía de acuerdo a su requerimiento únicamente.
+
+ii) en cada caso el refactoring que lo resuelve es:
+
+1) pull up field → en ppio debería permitir subir los atributos en común a una superclase de la que hereden las subclases
+2) encapsulate fields → se trata de poner como privados o protegidos los atributos que se encuentran públicos
+3) Para poder resolver el bad smell se harán dos refactoring: extract method y pull up method → El primero unifica el comportamiento común entre los 3 métodos. El segundo “sube” el método a la superclase desde la que se utilizará luego por herencia en cada subclase que lo requiera. 
+
+iii)
+1) creo una superclass Empleado que incluye los 3 atributos en común entre las 3 subclass. Luego hago que estas extiendan de Empleado para poder hacer uso de las mismas.
+
+abstract class Empleado {
+    public String nombre;
+    public String apellido;
+    public double sueldoBasico = 0;
+
+}
+
+public class EmpleadoTemporario extends Empleado {
+    public double horasTrabajadas = 0;
+    public int cantidadHijos = 0;
+    // ......
+    public double sueldo() {
+    return this.sueldoBasico + (this.horasTrabajadas * 500) + (this.cantidadHijos * 1000) 
+    - (this.sueldoBasico * 0.13);
+    }
+}
+
+public class EmpleadoPlanta extends Empleado {
+    public int cantidadHijos = 0;
+    // ......
+
+    public double sueldo() {
+        return this.sueldoBasico
+                + (this.cantidadHijos * 2000)
+                - (this.sueldoBasico * 0.13);
+    }
+}
+
+public class EmpleadoPasante extends Empleado {
+    // ......
+
+    public double sueldo() {
+        return this.sueldoBasico - (this.sueldoBasico * 0.13);
+    }
+}
+
+
+2) en este caso se hacen las dos cosas: primero ponemos como protegidas los atributos de la superclase, permitiendo el uso a las subclases. Por otra parte los atributos correspondientes a cada subclase se pondrán como privados, pero se deben crear getters y setter para los mismos.
+
+abstract class Empleado {
+    protected String nombre;
+    protected String apellido;
+    protected double sueldoBasico = 0;
+
+}
+
+public class EmpleadoTemporario extends Empleado {
+    private double horasTrabajadas = 0;
+    private int cantidadHijos = 0;
+    // ......
+    public double sueldo() {
+        return this.sueldoBasico + (this.horasTrabajadas * 500) + (this.cantidadHijos * 1000) - (this.sueldoBasico * 0.13);
+    }
+
+    public double getHorasTrabajadas(){
+        return this.horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(double horasT){
+        this.horasTrabajadas=horasT;
+    }
+
+    public int getCantidadHijos(){
+        return this.cantidadHijos;
+    }
+
+    public void setCantidadHijos (int cantH){
+        this.cantidadHijos=cantH;
+    }    
+}
+
+public class EmpleadoPlanta extends Empleado {
+    private int cantidadHijos = 0;
+    // ......
+
+    public double sueldo() {
+        return this.sueldoBasico + (this.cantidadHijos * 2000) - (this.sueldoBasico * 0.13);
+    }
+
+    public int getCantidadHijos(){
+        return this.cantidadHijos;
+    }
+
+    public void setCantidadHijos (int cantH){
+        this.cantidadHijos=cantH;
+    } 
+}
+
+public class EmpleadoPasante extends Empleado {
+    // ......
+
+    public double sueldo() {
+        return this.sueldoBasico - (this.sueldoBasico * 0.13);
+    }
+}
+
+3) Se considera que los tres métodos tienen un comportamiento en común. En este caso se hace el primer refactoring que es extract method: 
+pongo un único ejemplo, ya que lo considero un caso intermedio: todas las clases ahora repiten el método
+
+public double sueldoBasico() {
+        return this.sueldoBasico - (this.sueldoBasico * 0.13);
+    }
+
+mientras que se agrega un método en firma que será reescrito en cada clase que lo utiliza
+public double sueldo() {
+        return this.sueldoBasico - (this.sueldoBasico * 0.13) + (this.cantidadHijos * 	2000);
+    }
+
+Una vez hecho este paso intermedio en todas las clases, lo que se observa es un método repetido en todas las clases, momento en el que se debe 
+hacer un pull up method y dejarlo en la superclase para evitar la repetición de código. Quedaría:
+
+abstract class Empleado {
+    protected String nombre;
+    protected String apellido;
+    protected double sueldoBasico = 0;
+
+    public double sueldoBasico() {
+        return this.sueldoBasico - (this.sueldoBasico * 0.13);
+    }
+
+    public abstract sueldo();
+
+}
+
+public class EmpleadoTemporario extends Empleado {
+    private double horasTrabajadas = 0;
+    private int cantidadHijos = 0;
+    // ......
+    public double sueldo() {
+        return sueldoBasico() + (this.horasTrabajadas * 500) + (this.cantidadHijos * 1000);
+    }
+
+    public double getHorasTrabajadas(){
+        return this.horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(double horasT){
+        this.horasTrabajadas=horasT;
+    }
+
+    public int getCantidadHijos(){
+        return this.cantidadHijos;
+    }
+
+    public void setCantidadHijos (int cantH){
+        this.cantidadHijos=cantH;
+    }    
+}
+
+public class EmpleadoPlanta extends Empleado {
+    private int cantidadHijos = 0;
+    // ......
+
+    public double sueldo() {
+        return sueldoBasico + (this.cantidadHijos * 2000);
+    }
+
+    public int getCantidadHijos(){
+        return this.cantidadHijos;
+    }
+
+    public void setCantidadHijos (int cantH){
+        this.cantidadHijos=cantH;
+    } 
+}
+
+public class EmpleadoPasante extends Empleado {
+    // ......
+
+}
+
+Llegado a este punto, sigo encontrando BS: EmpleadoPlanta y EmpleadoPasante tienen en común un atributo que podría unificarse en una clase. 
+Esto reduciría parte del código repetido. Sucede lo mismo con la implementación de sueldo() que podría volver a unificarse (ASUMO QUE EL 
+HARDCODEO QUE PROPONEN ESTÁ EQUIVOCADO, Y LA IDEA ES QUE SEAN TODOS IGUALES). Luego de las modificaciones presentadas (en este caso son las 
+mismas que en la primera parte del ejercicio, vueltas a aplicar sobre la resultante de aquel) el código quedaría:
+
+abstract class Empleado {
+    protected String nombre;
+    protected String apellido;
+    protected double sueldoBasico = 0;
+
+    public double sueldoBasico() {
+        return this.sueldoBasico - (this.sueldoBasico * 0.13);
+    }
+
+    public abstract sueldoConHijos();
+
+}
+
+abstract class EmpleadoHabitual extends Empleado{
+    protected int cantidadHijos = 0;
+
+    public double sueldoConHijos(){
+        return sueldoBasico() + (this.cantidadHijos * 2000);
+    }
+
+    public abstract sueldo();   
+}
+
+public class EmpleadoTemporario extends EmpleadoHabitual{
+    private double horasTrabajadas = 0;
+    // ......
+    public double sueldo() {
+        return sueldoConHijos() + (this.horasTrabajadas * 500);
+    }
+
+    public double getHorasTrabajadas(){
+        return this.horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(double horasT){
+        this.horasTrabajadas=horasT;
+    }  
+}
+
+public class EmpleadoPlanta extends EmpleadoHabitual {
+    // ......
+    
+}
+
+public class EmpleadoPasante extends Empleado {
+    // ......
+
+}
+
+LA DUDA QUE ME QUEDA ES SI LAS DOS ÚLTIMAS CLASES NO SON LAZY CLASS O SI ES NECESARIO QUE QUEDEN ASÍ PARA PODER SEPARAR SUS FUNCIONALIDADES. 
+
+
+
